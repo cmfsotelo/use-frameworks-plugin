11dd22f35a9d03f239fb3283cfb10f707397fd22
Carlos Sotelo <carlos.sotelo7@gmail.com>
2020-01-02 12:40:35 +0000

    feat: swap to use_modular_headers instead of use_frameworks

--- src/ios/hooks/iOSBeforeInstall.js
+++ src/ios/hooks/iOSBeforeInstall.js
@@ -4,7 +4,7 @@ module.exports = function (context) {
 
     var platformRoot = path.join(context.opts.projectRoot, 'platforms/ios/cordova/lib');
     var podFile = path.join(platformRoot, 'Podfile.js');
-
+    
     if (fs.existsSync(podFile)) {
 
         fs.readFile(podFile, 'utf8', function (err, data) {
@@ -14,7 +14,7 @@ module.exports = function (context) {
             }
 
             data = data.replace("8.0", "9.3");
-            data = data.replace(" do\\n", " do\\n\\tuse_frameworks!\\n");
+            data = data.replace(" do\\n", " do\\n\\tuse_modular_headers!\\n");
             data = data.replace("Creating new Podfile in platforms/ios","Creating new Podfile in platforms/ios MY MY MY");
 
             fs.writeFile(podFile, data, 'utf8', function (err) {
